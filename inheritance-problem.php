ac24f7949664345e3fbaf2b1ba4b5702fbfc0226
Stevi greis Sianipar <151483216+SteviSianipar05@users.noreply.github.com>
2024-04-09 00:35:18 +0700

    Create inheritance-problem.php

---
+++ inheritance-problem.php
@@ -0,0 +1,34 @@
+<?php
+
+class Produk{
+    public $judul;
+    public $penulis;
+    public $penerbit;
+    public $harga;
+
+    public function __construct( $judul= "judul",$penulis = "penulis", $penerbit = "penerbit", $harga = 0 ){
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+    }
+
+    public function getLabel(){
+        return "$this->penulis, $this->penerbit";
+    }
+}
+    class CetakInfoProduk{
+        public function cetak( Produk $produk){
+            $str = "{$produk->judul} | {$produk->getLabel()} (Rp.$produk->harga)";
+            return $str;
+        }
+    }
+
+$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000);
+
+$produk2 = new Produk("Uncharted", "Neil Dructmann","Sony Computer",25000);
+
+// Komik : Masashi Kishimoto, Shonen Jump
+// Game : Neil Dructmann, Sony Computer
+// Uncharted | Neil Dructmann, Sony Computer (Rp.25000)
+
