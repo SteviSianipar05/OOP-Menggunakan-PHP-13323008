6b6f5ee9e0cc11f51aab9386974c387ed5d435d8
Stevi greis Sianipar <151483216+SteviSianipar05@users.noreply.github.com>
2024-04-12 01:13:00 +0700

    Update index.php

--- namespace/index.php
+++ namespace/index.php
@@ -2,15 +2,26 @@
 
 require_once 'App/init.php';
 
-$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
+// $produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
 
-$produk2 = new Game("Uncharted", "Neil Dructmann","Sony Computer",250000, 50);
+// $produk2 = new Game("Uncharted", "Neil Dructmann","Sony Computer",250000, 50);
 
-$cetakProduk = new CetakInfoProduk();
-$cetakProduk->tambahProduk ( $produk1);
-$cetakProduk->tambahProduk ( $produk2);
-echo $cetakProduk->cetak();
+// $cetakProduk = new CetakInfoProduk();
+// $cetakProduk->tambahProduk ( $produk1);
+// $cetakProduk->tambahProduk ( $produk2);
+// echo $cetakProduk->cetak();
 
-echo "<hr>";
+// echo "<hr>";
 
-new Coba();
+use App\Service\User as ServiceUser;
+use App\Service\User as ProdukUser;
+
+new ServiceUser();
+echo "<br>";
+new ProdukUser();
+
+// new User();
+
+// new App\Service\user();
+// echo "<hr>";
+// new App\Produk\user();
