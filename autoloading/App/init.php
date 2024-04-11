abd0959ed2364f07c5e89377441d012367bacbb7
Stevi greis Sianipar <151483216+SteviSianipar05@users.noreply.github.com>
2024-04-12 00:35:57 +0700

    Create init.php

---
+++ autoloading/App/init.php
@@ -0,0 +1,8 @@
+<?php
+require_once 'Produk/InfoProduk.php';
+require_once 'Produk/Produk.php';
+require_once 'Produk/Komik.php';
+require_once 'Produk/Game.php';
+require_once 'Produk/CetakInfoProduk.php';
+
+
