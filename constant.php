e3790efc0e605b9c320a2ff701b1805f8eec3aa8
Stevi greis Sianipar <151483216+SteviSianipar05@users.noreply.github.com>
2024-04-11 15:01:33 +0700

    Update constant.php

---
+++ constant.php
@@ -0,0 +1,18 @@
+ <?php
+
+// define ('NAMA', 'Stevi Greis Sianipar');
+// // define tidak dapat disimpan dalam sebuah class dan disimpan diluar
+
+// echo NAMA;
+// echo "<hr>";
+
+// const UMUR = 32;
+// // dapat disimpan kedalam class sehingga menggunakan konsep oriented
+// echo UMUR;
+
+
+class Coba {
+    define('NAMA', 'Stevi');
+}
+
+?>
