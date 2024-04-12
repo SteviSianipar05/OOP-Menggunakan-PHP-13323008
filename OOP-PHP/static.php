7b85c5c66e8f23c741d01d1e793d53ab511c9312
Stevi greis Sianipar <151483216+SteviSianipar05@users.noreply.github.com>
2024-04-12 21:21:25 +0700

    Create static.php

---
+++ OOP-PHP/static.php
@@ -0,0 +1,45 @@
+<?php
+
+// static selalu terikat dengan class
+
+// class ContohStatic {
+//     public static $angka = 1;
+
+//     public static function halo(){
+//         return "Halo " . self::$angka++ . " kali.";
+//     }
+// }
+
+// echo ContohStatic::$angka;
+// echo "<br>";
+// echo ContohStatic::halo();
+// echo "<hr>";
+// echo ContohStatic::halo();
+
+class Contoh {
+    public  static $angka = 1;
+    // nilai static akan tetap meskipun objeknya di instansiasi berulang kali
+    // Code menjadi procedural
+    public function halo(){
+        return "Halo " . self::$angka++ . " kali.";
+    }
+}
+
+$obj = new Contoh;
+echo $obj->halo();
+echo "<br>";
+echo $obj->halo();
+echo "<br>";
+echo $obj->halo();
+
+echo "<hr>";
+
+$obj2 = new Contoh;
+echo $obj2->halo();
+echo "<br>";
+echo $obj2->halo();
+echo "<br>";
+echo $obj2->halo();
+
+
+?>
