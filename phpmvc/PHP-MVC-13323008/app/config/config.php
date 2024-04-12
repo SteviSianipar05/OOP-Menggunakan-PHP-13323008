528ca8fbe4871c043c2c877710a59d7a3ef46f66
Stevi greis Sianipar <151483216+SteviSianipar05@users.noreply.github.com>
2024-04-12 23:14:37 +0700

    Create config.php

---
+++ phpmvc/PHP-MVC-13323008/app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+    define('BASEURL','http://localhost:8080/PABI/OOP-Menggunakan-PHP-13323008/phpmvc/PHP-MVC-13323008/public');
+
+    //DB
+    define ('DB_HOST','localhost:3306');
+    define ('DB_USER','root');
+    define ('DB_PASS','');
+    define('DB_NAME','phpmvc');
+
